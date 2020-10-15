8891eb94901780eeb9a8d39932e03d18d740e63a
lucasGomes01 <71887255+lucasGomes01@users.noreply.github.com>
2020-10-15 11:53:35 -0300

    Adicionar seletor da quantidade

--- src/app/produto/produto.page.ts
+++ src/app/produto/produto.page.ts
@@ -4,14 +4,13 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-produto',
   templateUrl: './produto.page.html',
   styleUrls: ['./produto.page.scss'],
+  
 })
+
 export class ProdutoPage implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  
 }
